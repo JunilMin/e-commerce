f386125665531200d87112f411c5c54b511c1339
JUNILMIN <jimin@aim.co.kr>
2024-04-12 09:43:04 +0900

    Test

---
+++ src/main/java/com/example/demo/model/Payment.java
@@ -0,0 +1,9 @@
+package com.example.demo.model;
+
+public class Payment {
+  int paymentId;
+  int orderId;
+  int userId;
+  int balance;
+
+}
---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+import org.springframework.data.annotation.Id;
+
+public class User {
+  int userId;
+  String account;
+  String address;
+  int contactNumber;
+}
