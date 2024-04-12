2f9d9ec93d7bf8bd743d7bea4bc9a6d302ef83d9
JUNILMIN <jimin@aim.co.kr>
2024-04-12 09:52:46 +0900

    Test

--- src/main/java/com/example/demo/model/Payment.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model;
-
-public class Payment {
-  int paymentId;
-  int orderId;
-  int userId;
-  int balance;
-
-}
--- src/main/java/com/example/demo/model/Product.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model;
-
-public class Product {
-  int productId;
-  int inventory;
-  int price;
-  String productName;
-
-}
